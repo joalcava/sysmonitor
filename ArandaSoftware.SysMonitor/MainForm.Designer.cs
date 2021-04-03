5201ae8ea8240cd3534c41909bc7c6cbfae2b6ca
Alejandro Cardona <alejandrocardona@outlook.com>
2021-04-02 22:21:09 -0500

    remove resource files utilization

--- ArandaSoftware.SysMonitor/MainForm.Designer.cs
+++ ArandaSoftware.SysMonitor/MainForm.Designer.cs
@@ -29,7 +29,6 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
             this.DescriptionLabel = new System.Windows.Forms.Label();
             this.GetDataBtn = new System.Windows.Forms.Button();
             this.LangBtn = new System.Windows.Forms.Button();
@@ -38,32 +37,47 @@
             // 
             // DescriptionLabel
             // 
-            resources.ApplyResources(this.DescriptionLabel, "DescriptionLabel");
+            this.DescriptionLabel.Location = new System.Drawing.Point(12, 9);
             this.DescriptionLabel.Name = "DescriptionLabel";
+            this.DescriptionLabel.Size = new System.Drawing.Size(560, 56);
+            this.DescriptionLabel.TabIndex = 1;
+            this.DescriptionLabel.Text = "This application allows you to capture the relevant information of the current co" +
+    "mputer (operating system, machine name, IP address, hard disk, RAM and processor" +
+    ")";
             // 
             // GetDataBtn
             // 
-            resources.ApplyResources(this.GetDataBtn, "GetDataBtn");
+            this.GetDataBtn.Location = new System.Drawing.Point(12, 52);
             this.GetDataBtn.Name = "GetDataBtn";
+            this.GetDataBtn.Size = new System.Drawing.Size(94, 23);
+            this.GetDataBtn.TabIndex = 0;
+            this.GetDataBtn.Text = "Get data";
             this.GetDataBtn.UseVisualStyleBackColor = true;
             this.GetDataBtn.Click += new System.EventHandler(this.GetDataBtn_Click);
             // 
             // LangBtn
             // 
-            resources.ApplyResources(this.LangBtn, "LangBtn");
+            this.LangBtn.Location = new System.Drawing.Point(506, 426);
             this.LangBtn.Name = "LangBtn";
+            this.LangBtn.Size = new System.Drawing.Size(66, 23);
+            this.LangBtn.TabIndex = 2;
+            this.LangBtn.Text = "EN";
             this.LangBtn.UseVisualStyleBackColor = true;
             this.LangBtn.Click += new System.EventHandler(this.button1_Click);
             // 
             // DataLbl
             // 
-            resources.ApplyResources(this.DataLbl, "DataLbl");
+            this.DataLbl.Location = new System.Drawing.Point(12, 87);
             this.DataLbl.Name = "DataLbl";
+            this.DataLbl.Size = new System.Drawing.Size(560, 323);
+            this.DataLbl.TabIndex = 0;
+            this.DataLbl.Text = "...";
             // 
             // MainForm
             // 
-            resources.ApplyResources(this, "$this");
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 461);
             this.Controls.Add(this.DataLbl);
             this.Controls.Add(this.LangBtn);
             this.Controls.Add(this.GetDataBtn);
--- ArandaSoftware.SysMonitor/MonitoringDataStringBuilder.cs
+++ ArandaSoftware.SysMonitor/MonitoringDataStringBuilder.cs
@@ -14,29 +14,29 @@ namespace ArandaSoftware.SysMonitor
 
         public static string Build(MonitoringData data)
         {
-            var resources = new ComponentResourceManager(typeof(MainForm));
+            //var resources = new ComponentResourceManager(typeof(MainForm));
             var ramUsagePercentage = (data.RamUsage / data.RamCapacity) * 100;
 
             var stringBuilder = new StringBuilder()
-                .AppendJoin(" ", BulletChar, resources.GetString("OperativeSystem"))
+                .AppendJoin(" ", BulletChar, "Operative System")
                 .AppendJoin(" ", ":", data.OsName)
                 .AppendLine()
-                .AppendJoin(" ", BulletChar, resources.GetString("MachineName"))
+                .AppendJoin(" ", BulletChar, "Machine Name")
                 .AppendJoin(" ", ":", data.HostName)
                 .AppendLine()
-                .AppendJoin(" ", BulletChar, resources.GetString("IpAddress"))
+                .AppendJoin(" ", BulletChar, "IP Address")
                 .AppendJoin(" ", ":", data.IpAddress)
                 .AppendLine()
-                .AppendJoin(" ", BulletChar, resources.GetString("Disk"))
+                .AppendJoin(" ", BulletChar, "Hard Disk")
                 .AppendJoin(" ", ":", data.DiskCapacity, "/", data.DiskUsage)
                 .AppendLine()
-                .AppendJoin(" ", BulletChar, resources.GetString("Ram"))
+                .AppendJoin(" ", BulletChar, "RAM")
                 .AppendJoin(" ", ":", data.RamCapacity, "MB", "/", ramUsagePercentage.ToString("0.##"), "%")
                 .AppendLine()
-                .AppendJoin(" ", BulletChar, resources.GetString("CPU"))
+                .AppendJoin(" ", BulletChar, "CPU")
                 .AppendJoin(" ", ":", data.CpuName, "/", data.CpuUsage)
                 .AppendLine()
-                .AppendJoin(" ", BulletChar, resources.GetString("Date"))
+                .AppendJoin(" ", BulletChar, "Report Date")
                 .AppendJoin(" ", ":", data.DateCreated)
                 .AppendLine();
 
